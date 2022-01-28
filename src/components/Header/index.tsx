d18010133b673a4d1cb20accebfcb477fce68018
Mathias Hahner <mathias.hahner.123@gmail.com>
2022-01-28 14:12:00 -0300

    creation of the header and player components and added json with fake data application

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,20 @@
+import format from 'date-fns/format'
+import ptBR from 'date-fns/locale/pt-BR'
+
+import styles from './styles.module.scss'
+
 export function Header() {
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBR
+  })
+
   return (
-    <header>
-      
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Jabyer" />
+
+      <p>Rock é vida!</p>
+
+      <span>{currentDate}</span>
     </header>
   )
 }
